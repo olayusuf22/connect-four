a2da80691fc5c712c6f26b179e8174e4944b60f3
tech-ola <latuborsun@gmail.com>
2024-07-26 13:26:34 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 /*----- constants -----*/
+const COLORS = {
+    '1': 'purple',
+    '-1': 'orange',
+    'null': 'white',
+};
 
 
 /*----- state variables -----*/
-
+let board;
+let turn;
+let winner;
 
 /*----- cached elements  -----*/
 
@@ -11,4 +18,37 @@
 
 
 /*----- functions -----*/
+init();
 
+// Initialize all state, then call render()
+function init() {
+    // you can visualixe the board array's mapping
+    // to the DOM by rotating the board array 90 deg counter-clockwise
+    board = [
+      [null, null, null, null, null, null,], // column 0  
+      [null, null, null, null, null, null,], // column 1 
+      [null, null, null, null, null, null,], // column 2 
+      [null, null, null, null, null, null,], // column 3 
+      [null, null, null, null, null, null,], // column 4 
+      [null, null, null, null, null, null,], // column 5
+      [null, null, null, null, null, null,], // column 6
+   ];
+   turn = 1;
+   winner = null;
+   render();
+}
+
+// visualize all state and other info (like messaging)
+// in the DOM
+function render() {
+    renderBoard();
+}
+
+function renderBoard () {
+  board.forEach(fucntion(colArr, colIdx) {
+    colArr.forEach(function(cellVal, rowIdx) {
+        const cellEL = document.getElementById(`c${colIdx}r${rowIdx}`);
+        cellEL.style.backgroundColor = COLORS[];
+    });
+  });
+}
